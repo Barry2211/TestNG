fd382c65381d4b59261655e86daff79d3984eeeb
BARRY <ganeshan.shakthi@gmail.com>
2025-06-18 11:21:18 +0530

    windowHandles raw code

--- src/test/java/org/windows/Windows.java
+++ src/test/java/org/windows/Windows.java
@@ -8,22 +8,34 @@ import java.util.List;
 import java.util.Set;
 
 import org.baseclass.DriverUtils;
-import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class Windows extends DriverUtils{
 
+	Set<String> landingPages =  new LinkedHashSet<>();
+	List<String> windowList = new ArrayList<>();
 	@Test
 	public void handling() {
 		driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://adactinhotelapp.com/");
 		String homePage = driver.getWindowHandle();
+		windowList.add(homePage);
 		javascript(newWindow);
+		landingPages =  driver.getWindowHandles();
+		for(String page:landingPages) {
+			if(!windowList.contains(page)) {
+				windowList.add(page);
+			}
+		}
 		javascript(newWindow);
-		Set<String> landingPages =  new LinkedHashSet<>(driver.getWindowHandles());
-		List<String> windowList = new ArrayList<>(landingPages);
+		landingPages =  driver.getWindowHandles();
+		for(String page:landingPages) {
+			if(!windowList.contains(page)) {
+				windowList.add(page);
+			}
+		}
 		driver.switchTo().window(windowList.get(1));
 		driver.get("https://www.facebook.com/");
 		driver.switchTo().window(windowList.get(2));
